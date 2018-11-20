11ef9e58a04c4a9222fd2b4a09ba09b751319a17
LiTao <litao@tuya.com>
2018-11-20 15:03:26 +0800

    support sdk 15.2.0 for nrf52840

--- tool/run.sh
+++ tool/run.sh
@@ -8,14 +8,16 @@ SDK_PATH=$PROJECT_ROOT/sdk
 
 ARM_GCC_PATH=$TOOLS_PATH/gcc-arm-none-eabi-5_4-2016q3
 NRF5X_COMMAND_LINE_PATH=$TOOLS_PATH/nRF5x-Command-Line-Tools_9_7_3
-NRF5_SDK_12_3_0_PATH=$SDK_PATH/nRF5_SDK_12.3.0_d7731ad
+NRF5_SDK_12_3_0_PATH=$SDK_PATH/nRF5_SDK_12.3.0_d7731ad 
+NRF5_SDK_15_2_0_PATH=$SDK_PATH/nRF5_SDK_15.2.0_9412b96
 
 ARM_GCC_LINK=https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
 NRF5X_COMMAND_LINE_LINK=https://www.nordicsemi.com/eng/nordic/download_resource/51392/29/85293435/94917
 NRF5_SDK_12_3_0_LINK=https://www.nordicsemi.com/eng/nordic/download_resource/54291/56/91735559/32925
+NRF5_SDK_15_2_0_LINK=https://www.nordicsemi.com/eng/nordic/download_resource/59012/94/74606268/116085
 
-
-function tool(){
+#--------------------------------------------------------------------------
+function install_arm_gcc(){
     echo "> install arm gcc tool ..."
     if [ ! -d $ARM_GCC_PATH ]; then 
         pack=gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2 
@@ -23,7 +25,9 @@ function tool(){
 		tar -xjvf $pack 
 		rm -rf $pack 
 	fi
+}
 
+function install_nrf5x_command_line(){
     echo "> install nRF5x command line tool ..."
     if [ ! -d $NRF5X_COMMAND_LINE_PATH ]; then 
         pack=nRF5x-Command-Line-Tools_9_7_3_Linux-x86_64.tar
@@ -32,7 +36,9 @@ function tool(){
         tar -xvf $pack -C $NRF5X_COMMAND_LINE_PATH
         rm -rf $pack 
     fi
+}
 
+function install_nrf5_sdk_12_3_0(){
     echo "> install nRF5 SDK 12.3.0 ..."
     if [ ! -d $NRF5_SDK_12_3_0_PATH ]; then 
         if [ ! -d $SDK_PATH ]; then
@@ -51,7 +57,37 @@ function tool(){
     posix_file=$NRF5_SDK_12_3_0_PATH"/components/toolchain/gcc/Makefile.posix"
     echo "GNU_INSTALL_ROOT := $t_arm_gcc_path" > $posix_file
     echo "GNU_VERSION := 5.4.1" >> $posix_file
-    echo "GNU_PREFIX := arm-none-eabi" >> $posix_file
+    echo "GNU_PREFIX := arm-none-eabi" >> $posix_file   
+}
+
+function install_nrf5_sdk_15_2_0(){
+    echo "> install nRF5 SDK 15.2.0 ..."
+    if [ ! -d $NRF5_SDK_15_2_0_PATH ]; then 
+        if [ ! -d $SDK_PATH ]; then
+            mkdir -p $SDK_PATH
+        fi
+
+        pack=nRF5_SDK_15.2.0_9412b96
+        wget -O $pack.zip $NRF5_SDK_15_2_0_LINK 
+        unzip -X $pack.zip
+        rm -rf $pack.zip
+        mv $pack $SDK_PATH
+    fi
+
+    echo "> update the *.posix file, when the project root is changed ..."
+    t_arm_gcc_path=`pwd`/gcc-arm-none-eabi-5_4-2016q3
+    posix_file=$NRF5_SDK_15_2_0_PATH"/components/toolchain/gcc/Makefile.posix"
+    echo "GNU_INSTALL_ROOT := $t_arm_gcc_path" > $posix_file
+    echo "GNU_VERSION := 5.4.1" >> $posix_file
+    echo "GNU_PREFIX := arm-none-eabi" >> $posix_file   
+}
+#--------------------------------------------------------------------------
+
+function tool(){
+    install_arm_gcc;
+    install_nrf5x_command_line;
+    install_nrf5_sdk_12_3_0;
+    install_nrf5_sdk_15_2_0;
 }
 
 function clean(){
@@ -59,6 +95,7 @@ function clean(){
     rm -rf $ARM_GCC_PATH
     rm -rf $NRF5X_COMMAND_LINE_PATH
     rm -rf $NRF5_SDK_12_3_0_PATH
+    rm -rf $NRF5_SDK_15_2_0_PATH
 }
 
 
